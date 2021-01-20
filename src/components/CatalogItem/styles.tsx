e90731ebe24a52bd489652a51b47d200c7d4c8d5
mauriani <maurianim@gmail.com>
2021-01-20 17:01:56 -0300

    alterações

--- src/components/CatalogItem/styles.tsx
+++ src/components/CatalogItem/styles.tsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin: 50px 0;
+export const Container = styled.ul`
+  max-width: 1020px;
+  margin: 0 auto;
+  padding: 0 20px 50px;
+
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 20px;
+  list-style: none;
+
   li {
     display: flex;
     flex-direction: column;
