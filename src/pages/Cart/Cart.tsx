54e58b7b8031d25a54f5c9688f5ea707fbd2814d
mauriani <maurianim@gmail.com>
2021-02-11 17:18:45 -0300

    altrs

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,84 +1,91 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MdRemoveCircleOutline, MdAddCircleOutline } from "react-icons/md";
-
+import Header from "../../components/Header";
 import { IState } from "../../store";
 import {
+  ICartItem,
   ICartState,
   IProduct,
-  ICartItem,
 } from "../../store/modules/cart/types";
 
 import { Container, ProductTable } from "./styles";
+import { addProductToCartRequest } from "../../store/modules/cart/actions";
 
 const Cart: React.FC = () => {
   const cart = useSelector<IState, ICartState>((state) => state.cart);
 
   console.log(cart);
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  function increment(product: IProduct) {
-    console.log(product);
+  function increment(quantity: ICartItem, product: IProduct) {
+    dispatch(addProductToCartRequest(product));
+    console.log(quantity, product.id);
   }
-  function decrement(product: IProduct) {
-    console.log(cart);
+  function decrement(quantity: ICartItem, product: IProduct) {
+    console.log(quantity, product.id);
   }
 
   return (
-    <Container>
-      <ProductTable>
-        <table>
-          <thead>
-            <tr>
-              <th>Produto</th>
-              <th>Preço</th>
-              <th>Quantidade</th>
-              <th>Subtotal</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.items.map((item) => (
-              <tr key={item.product.id}>
-                <td>
-                  <img src={item.product.image} alt={item.product.title} />
-                </td>
-
-                <td>
-                  <strong>{item.product.title}</strong>
-                  <span>{item.product.priceFormatted}</span>
-                </td>
-
-                <td>
-                  <div>
-                    <button
-                      type="button"
-                      onClick={() => decrement(item.product)}
-                    >
-                      <MdRemoveCircleOutline size={20} color="#7159c1" />
-                    </button>
-
-                    <input type="number" readOnly value={item.quantity} />
-
-                    <button
-                      type="button"
-                      onClick={() => increment(item.product)}
-                    >
-                      <MdAddCircleOutline size={20} color="#7159c1" />
-                    </button>
-                  </div>
-                </td>
-
-                <td>
-                  <span>{(item.product.price * item.quantity).toFixed(2)}</span>
-                </td>
+    <>
+      <Header />
+      <Container>
+        <ProductTable>
+          <table>
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+                <th>Subtotal</th>
+                <th>Total</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </ProductTable>
-    </Container>
+            </thead>
+            <tbody>
+              {cart.items.map((item) => (
+                <tr key={item.product.id}>
+                  <td>
+                    <img src={item.product.image} alt={item.product.title} />
+                  </td>
+
+                  <td>
+                    <strong>{item.product.title}</strong>
+                    <span>{item.product.priceFormatted}</span>
+                  </td>
+
+                  <td>
+                    <div>
+                      <button
+                        type="button"
+                        onClick={() => decrement(item, item.product)}
+                      >
+                        <MdRemoveCircleOutline size={20} color="#7159c1" />
+                      </button>
+
+                      <input type="number" readOnly value={item.quantity} />
+
+                      <button
+                        type="button"
+                        onClick={() => increment(item, item.product)}
+                      >
+                        <MdAddCircleOutline size={20} color="#7159c1" />
+                      </button>
+                    </div>
+                  </td>
+
+                  <td>
+                    <span>
+                      {(item.product.price * item.quantity).toFixed(2)}
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </ProductTable>
+      </Container>
+    </>
   );
 };
 
